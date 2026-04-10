18f552a72eb50323c9a41101f9e55ff05eae2630
Marko Mustonen <marko.mustonen.89@gmail.com>
2026-04-10 13:44:01 +0300

    Muutettu pom-tiedostoa, poistettu maven.compiler.source sekä maven.compiler.target ylimääräisenä ja muutettu release-arvo Java-julkaisuun 21. Muokattu nextInt lauseketta, parametreihin määritelty suoraan lukuväli (1, 7);. Otettu käyttöön uudempi switch-lauseke NopallaArvontaControllerissa, joka hyödyntää nuolifunktiota ja jättää pois break; kohdan.

--- javapelihalli/src/main/java/com/github/javapelihalli/NopallaArvontaController.java
+++ javapelihalli/src/main/java/com/github/javapelihalli/NopallaArvontaController.java
@@ -19,59 +19,52 @@ public class NopallaArvontaController {
         Random random = new Random();
         int[] nopat = new int[3];
 
-         nopat[0] = random.nextInt(6) + 1;
-         nopat[1] = random.nextInt(6) + 1;
-         nopat[2] = random.nextInt(6) + 1;
+         nopat[0] = random.nextInt(1, 7);
+         nopat[1] = random.nextInt(1, 7);
+         nopat[2] = random.nextInt(1, 7);
 
 
          System.out.println("Nopan arvot olivat:");
 
-         for(int i = 0; i<nopat.length; i++){
+          for(int i = 0; i<nopat.length; i++){
 
             switch (nopat[i]) {
-
-                case 1: 
-                        System.out.println(" -------");
+                
+                case 1 -> {System.out.println(" -------");
                            System.out.println("|       |");
                            System.out.println("|   o   |");
                            System.out.println("|       |");
-                           System.out.println(" -------");
-                           break;
+                           System.out.println(" -------");}
 
-                case 2:   
-                        System.out.println(" -------");
+                case 2 -> {System.out.println(" -------");
                           System.out.println("|o      |");
                           System.out.println("|       |");
                           System.out.println("|      o|");
-                          System.out.println(" -------");
-                          break;
+                          System.out.println(" -------");}
 
-                case 3: System.out.println(" -------");
+                case 3 -> {System.out.println(" -------");
                           System.out.println("|o      |");
                           System.out.println("|   o   |");
                           System.out.println("|      o|");
-                          System.out.println(" -------");
-                          break;
+                          System.out.println(" -------");}
 
-                case 4: System.out.println(" -------");
+                case 4 -> {System.out.println(" -------");
                           System.out.println("|o     o|");
                           System.out.println("|       |");
                           System.out.println("|o     o|");
-                          System.out.println(" -------");
-                          break;
+                          System.out.println(" -------");}
 
-                case 5:  System.out.println(" -------");
+                case 5 -> {System.out.println(" -------");
                           System.out.println("|o     o|");
                           System.out.println("|   o   |");
                           System.out.println("|o     o|");
-                          System.out.println(" -------");
+                          System.out.println(" -------");}
 
-                case 6:  System.out.println(" -------");
+                case 6 -> {System.out.println(" -------");
                           System.out.println("|o     o|");
                           System.out.println("|o     o|");
                           System.out.println("|o     o|");
-                          System.out.println(" -------");
-                          break;
+                          System.out.println(" -------");}
                 
             }
          }
